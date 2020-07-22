cb2f8d5fd252357a7bca42465e4f5e911f3f297b
Nityanand Rai <n.n.r1204@gmail.com>
2020-07-22 18:25:12 +0530

    Add files via upload

---
+++ success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '[success-alert]',
+    templateUrl: './success-alert.component.html',
+    styles: [`
+        button {
+            margin-top: 10px;
+         }
+    `]
+})
+
+export class SuccessAlertComponent{
+
+}
